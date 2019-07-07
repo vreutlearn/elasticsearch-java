00ab61466d87370fcafd391d467ffd7e3a438502
vreut <victor.reut@itrexgroup.com>
2019-07-07 21:50:04 +0300

    elastic search demo via rest usage

---
+++ README.md
@@ -0,0 +1,11 @@
+# es-java-api
+
+Project stack versions:
+
+ - Java 1.8 (152)
+ - ElasticSearch: 7.1
+
+Sample APIs of using ElasticSearch Official [Java REST Client](https://www.elastic.co/guide/en/elasticsearch/client/java-rest/7.1/index.html).
+
+Specifically this example shows applying the [High level REST Client](https://www.elastic.co/guide/en/elasticsearch/client/java-rest/7.1/java-rest-high.html)
+
---
+++ src/main/java/by/itrex/elasticsearch/api/IndexApi.java
@@ -0,0 +1,48 @@
+package by.itrex.elasticsearch.api;
+
+import by.itrex.elasticsearch.Application;
+import org.apache.http.Header;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.rest.RestStatus;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * Samples of using elasticsearch index API requests.
+ */
+public class IndexApi {
+
+    private final RestHighLevelClient client;
+    private final String index;
+    private final String type;
+
+    public IndexApi(final RestHighLevelClient client, final String index, final String type) {
+        this.client = client;
+        this.index = index;
+        this.type = type;
+    }
+
+    /**
+     * Create an index, type and document post with custom id.
+     * As request body set json string.
+     */
+    public void exampleWithJsonRequest() throws IOException {
+        final String id = UUID.randomUUID().toString();
+
+        final IndexRequest indexRequest = new IndexRequest(index, type, id);
+        final String jsonString = "{" +
+                "\"user\":\"Victor Reut\"," +
+                "\"message\":\"trying out Elasticsearch\"" +
+                "}";
+        indexRequest.source(jsonString, XContentType.JSON);
+
+        final Header[] headers = Application.prepareAuthHeader();
+        final IndexResponse response = client.index(indexRequest, headers);
+        assert response.status().equals(RestStatus.OK);
+
+    }
+}
